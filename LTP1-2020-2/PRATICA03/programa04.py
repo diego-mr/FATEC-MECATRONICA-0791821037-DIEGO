bbafac885aefc55edae76d0b5fb146fe626b172f
diego-mr <70581607+diego-mr@users.noreply.github.com>
2020-09-08 09:16:41 -0300

    Create programa04.py

---
+++ LTP1-2020-2/PRATICA03/programa04.py
@@ -0,0 +1,4 @@
+#Pede para o usuario um valo em cm e converte para metros
+valor_cm = float(input('informe um valor em cm:'))
+valor_m = valor_cm / 100
+print('valor convertido em metros:', valor_m)
