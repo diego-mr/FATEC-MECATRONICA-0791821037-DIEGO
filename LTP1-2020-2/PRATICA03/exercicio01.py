d0519fd5aa73fe36c9a58c8e9a13fade54f3b4d9
diego-mr <70581607+diego-mr@users.noreply.github.com>
2020-09-08 23:14:04 -0300

    Update exercicio01.py

--- LTP1-2020-2/PRATICA03/exercicio01.py
+++ LTP1-2020-2/PRATICA03/exercicio01.py
@@ -1,1 +1,4 @@
-
+#Escreva um programa permita o usuário informa uma temperatura em graus Celsius e converta ela para graus Kelvin 
+valor_celsius = float(input('inserir o valor do grau em celsius:'))
+valor_kelvin = 273.15 + valor_celsius 
+print('valor convertido em kelvin', valor_kelvin)
