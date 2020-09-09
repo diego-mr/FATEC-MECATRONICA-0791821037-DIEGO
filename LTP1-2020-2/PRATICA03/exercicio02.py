5fc2df5f0c1df457a3a46b2297fab7773f4c88c1
diego-mr <70581607+diego-mr@users.noreply.github.com>
2020-09-08 23:26:09 -0300

    Create exercicio02.py

---
+++ LTP1-2020-2/PRATICA03/exercicio02.py
@@ -0,0 +1,4 @@
+#Escreva um programa permita o usuário informa uma temperatura em graus Celsius e converta ela para graus Fahrenheit  
+valor_celsius = float(input('inserir o valor do grau em celsius:'))
+valor_Fahrenheit = 1.8 * valor_celsius + 32
+print('valor convertido em Fahrenheit', valor_Fahrenheit)
